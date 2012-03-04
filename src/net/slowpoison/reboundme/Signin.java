5339f36ce36e4baed9955f21c99e9382126fb937
Vishal Verma <v@slowpoison.net>
2012-03-03 22:23:10 -0800

    First check-in

---
+++ src/net/slowpoison/reboundme/Signin.java
@@ -0,0 +1,29 @@
+package net.slowpoison.reboundme;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+
+public class Signin extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.signin);
+	}
+	
+	public void signin(View v) {
+//		startActivity(b);
+		Intent doSignin = new Intent(this, WebActivity.class);
+		startActivityForResult(doSignin, 1);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+}
---
+++ src/net/slowpoison/reboundme/WebActivity.java
@@ -0,0 +1,41 @@
+package net.slowpoison.reboundme;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class WebActivity extends Activity {
+//	private Uri authUrl = Uri.parse("https://runkeeper.com/apps/authorize?client_id=bdf6d860d6964992a766c4405d7e5638&response_type=code&redirect_uri=javascript:window.close()");
+	private String authUrl = "https://runkeeper.com/apps/authorize?client_id=bdf6d860d6964992a766c4405d7e5638&response_type=code&redirect_uri=javascript:close()";
+//	private String authUrl = "http://www.google.com";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.webview);
+		
+		final WebView wv = (WebView) findViewById(R.id.webview);
+		wv.getSettings().setJavaScriptEnabled(true);
+		wv.loadUrl(authUrl);
+		wv.setWebViewClient(new WebViewClient() {
+
+			@Override
+			public void onPageStarted(WebView view, String url, Bitmap favicon) {
+				// TODO Auto-generated method stub
+				super.onPageStarted(view, url, favicon);
+				Toast.makeText(WebActivity.this, "Starting with url " + url, Toast.LENGTH_SHORT).show();
+				wv.stopLoading();
+			}
+
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				super.onPageFinished(view, url);
+				Toast.makeText(WebActivity.this, "Done with url " + url, Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
+}
