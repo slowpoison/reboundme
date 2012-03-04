6406b58187fea58067685396ff96fcd86c315342
Vishal Verma <v@slowpoison.net>
2012-03-04 00:56:01 -0800

    Sign-in works. Gets access token back

---
+++ src/net/slowpoison/reboundme/Home.java
@@ -0,0 +1,14 @@
+package net.slowpoison.reboundme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Home extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home);
+	}
+
+}
